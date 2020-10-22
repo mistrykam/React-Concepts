e96aa7a4179d2224ed67115741b99f9aa548bad5
Kam Mistry <mistrykam@gmail.com>
2020-10-22 09:36:21 -0400

    hook-counter-1 should also update an object

--- src/components/hook-counter-1.js
+++ src/components/hook-counter-1.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 
 function HookCounterOne() {
   const [counter, setCount] = useState(0);
+  const [person, setPerson] = useState({ name: "John Smith", points: 1 });
 
   // setCount() <-- what ever data is passed in will be assigned to 'counter'
   //    eg. setCount(counter + 1)
@@ -10,9 +11,17 @@ function HookCounterOne() {
 
   return (
     <div className="container">
-      <p>Hook Counter One</p>
-      <h3>{counter}</h3>
-      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <div>
+        <p>Hook Counter One</p>
+        <h3>{counter}</h3>
+        <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      </div>
+      <div>
+        <h3>
+          {person.name} has {person.points}
+        </h3>
+        <button onClick={() => setPerson((prev) => ({ ...prev, points: prev.points + 1 }))}>Add Points</button>
+      </div>
     </div>
   );
 }
